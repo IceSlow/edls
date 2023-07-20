a328e516d95482922a363fe1d8ef1d3b37d111f2
Ice Slow <jhampier23edu@gmail.com>
2023-07-19 21:17:28 -0500

    Creamos constantes para nuestro archivos y extenciones

---
+++ edls.go
@@ -0,0 +1,24 @@
+package main
+
+// file types
+const (
+	fileRegular int = iota
+	fileDirectory
+	fileExecutable
+	fileCompress
+	fileImage
+	fileLink
+)
+
+// file extensión
+const (
+	exe  = ".exe"
+	deb  = ".deb"
+	rar  = ".rar"
+	zip  = ".zip"
+	tar  = ".tar"
+	gz   = ".gz"
+	png  = ".png"
+	jpg  = ".jpg"
+	gifs = ".gifs"
+)
